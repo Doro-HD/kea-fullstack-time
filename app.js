f0664f20133a9864d8a74347f1935b2dc6a48061
David <doro.hd@skiff.com>
2023-03-02 23:07:48 +0100

    Attempt to fix deployment static folder

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const path = require('path')
 const express = require('express')
 const app = express()
 
-app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/frontpage/frontpage.html')
 })
 
-module.exports = app
+app.listen(8080)
+// module.exports = app
