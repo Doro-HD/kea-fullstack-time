91ea9c168c12dabf537f387f89dc79a9151ed1c6
David <doro.hd@skiff.com>
2023-03-02 14:12:01 +0100

    Add colours to display state of words, highscore and allow for play again

--- public/frontpage/frontpage.js
+++ public/frontpage/frontpage.js
@@ -1,29 +1,69 @@
+window.addEventListener('load', () => {
+  const infoh3 = document.querySelector('h3#info')
+  infoh3.textContent = 'Start typing to begin the game.'
+
+  idleState()
+
+})
 //setup the words
 const wordsString = "låse knække tvær privat skose ilt stimulus bynke pirat få hår ar pulse data føde virulent hofte skot rede sø væve for ravine pilsner kompetent manufaktur bimle strimmel heraldik grat fiks autarki kure garrotte svingle fætter beskeden die gæste drøje inkasso stunde sulfit Herning borte levre naver pedal factor kjole søle bikse mætte Rønne tysk gryn kærte flyde trods råge mager kambrium rynke logo hive honning mæske trave dug dårlig grund tribut rus handel lig kalve krable pelikan kravat sprælsk løjpe tue måle skaft plante refugie svip feste emalje kolloid gen vade stenografi ankel bånde sløse brint skide impuls menu rutte sago røge smult strø gnidsel drik hage stak fingre strejf fissil gestalte sukre vrå drejl gruse pjalt sladre baldrian gjaldre klukke såh trold flyve tazet musvit søbe bæk grafolog skalle føj bouclé jod kimære ego røbe slips elm firs sive havarti excellence lud smægte ilter vejre rusk stase kristne bål æde lykke kortisol klatte hylde bank paria hænde chateau gåde blotte litotes sprøjt nota tavs spyt smalte puge virolog salep gammen krebs falde kælke vippe angst pris smerte rafte hæge fnat føre Bramming skrible jubilar slig klore tres live tyst tumle sprut intakt på famle farisæer plette ol skænde minde kargo kitsch snorke blu floskel pamflet dekorum ditten fad metallurgi hungre plaf potentil melasse fod svigt klusil biocid folde foster hin Botswana Salling bruse spilde svinde spån pensle nyde rime viola klø koloni køje trakeotomi klynge polt mops kalorius bobine mistral burlesk byde bitte pris lede hulke riv lidse ridt finsk fritte kætter kommode tarok rinde lag periferi snippe finit hak fællig hu pastrami kahyt kvote ovn hutle zeta makulatur skræve skulke skovle patrulje visne sigma male nøkke pisse plumre galla brunst klæbe dranker huske dyb perifrase strejfe vanke dominant hu handske takle kolonne fjant observant valsk drøfte bunde næste fag ny sile verbal vinyl midje kir vånde sved skitse sprætte justits liv dryade satire sari canasta logge dele hænde trave skole skrå fenol adresse snubbe fiol hamle klima måne titte værke glans mikse krejle gafle aske drægtig topas trone komplot permanent glut servitut syssel dure gylpe fenyl difteri parasit sjæl snage veto gump græsse lade kanut replik nu prud sats fis regel palisade løs metyl moneter fart ål svigte stylte drikke faun rokade kollapse Jelling kaskade vrinsk sarkasme gane øst force mjav høj frister grant bøjle hale bap karse tapper bak hiv noppe oceanolog gære karbon Danmark kamille svovle lager ros pagt rock hej rå lemming kalk kuld pral tøffe skravl disk bor visk invalid skrige brik krage piraya tutte fantasme diva filter flintre styrte skuldre trusse binokular sværte slunken arabesk retræte smelte sinke tusch lipid flekse doble lyve svejse si poppe tjald balsam serviet ark tulle sæt stræb pande mås gnalling lur svinge stevnsk botanik svide slibe quisling vride vægt mask bid type havre drop dykke spinne passe sigte rabundus grissini planke fremme lux gazelle kollega spore glamme lange ælde lide rubrik rasle skimme mosle nat holdt hitte erindre viril maori mobil smæk gælde exe kran ende gemmolog rejse hyle Merkur id sinde glibe plesken bræge sniffe springe skur snobbe tyre blanke løsne emblematik tak prise fimse kneb simre verfe sang ragout pirol lampret skabe snakke lide stemple guttural krater medalje ski spydig vejle drosle dyb kræs briks skabe spange infarkt bakke sjavs hibiscus swing mokke svige fortynde trombone drop merian sko flette pryde kaliber hundse gjalde hvene akut diæter plet vrang brokke volt skjold charme stativ tvivle novice fjerne pløs mælk proppe vølve krukke nitrit cirrose hor plumpe stødt oppe franc pilaster fure pæl vink salut libertiner Prøjsen ajle torv pianino pile smakke palatal stadig merkantil deponi bulimi tude nitte over dysse vind naiv rask maltose sprød mulle dreje opak sæde klasse huldre ryge sekt flamme centre grøble tjans tynge knase jeremiade skrat slække vulkanolog lige pamper men konsilium vræle funkle pine skinger helle hævn strøg rim kemo øde centrum fals grammatik lut spat mør kapriol få ur krans ruin sin Milano skvætte marina tavle spot sitre dryppe hørme pode vært tun stramme facil skabiose kusk eje tinte mejsel trope glide skralde bulter dæksel servitrice hjul skryde se rysler kanonade skandale manna plakette dyk råd liste visum kommune sauna under bodega diabetes drue glæde drømme liguster knag hybrid grosserer oval tinde kateter zone bole lunde laps tætne debil piment harve flora løfte slynge bøvle gelejde støv så prompte tjalk huri bryozo peseta yver gøde lumske brasen ase kommen drone syge køle riviera kombinatorik etikette heppe kommando pift elev hede score knob tarif ditto promille fordakning terzine gul skrumpe koma tremolo dolke spidde knald bejdse stride plusse urdu takke vunde lyne læge statur snor arkade munter kny mange spin forte pøbel flet talent våde gnide koster zulu hingst reprise montrice stund blomme bauxit tragt lask flugt batte rage bramme grunde vænge katalog pro synftig varme pige kræve vams kabale domicil blod endokrin strø svastika værk skride tømme knogle vægte rase fortætte sund røsken lilje ranke grin tusk knyst vold bakse Mali dejse kime ræbe orne fjerte lysne ege rotte blomst mødding rive krumme asp benzol raspe pilaf tralle bekræfte toupé dement faktoid tut lind prale teori plus vippe sidst muræne rense skyr mose credo lunke gæk scripte blære cirkle strid stil vise løse tølt smykke pligt fjog skurv kælve okker tema batalje slendrian fenne valid pæn klumpe mester frodig passiar forsytia sande haspe facet nisse violet møller hæk emir kyrie lig snude banke pullimut nørkle svang friske musik ørred gak klimaks gæt inspektrice diurese tapir knitre jan fise glat mildne angre enke divisor ting lampet fuse kvæde spatel legation tre slat frynse fikse enklave gæv tit rær hel ild vånde zygote skolde slip boks bøde vægre hinke Lisboa myte analyse ortodoks telt sænk hjule himle træn domæne bylte maske hynde din spag kløgt spække stejle slæng cymbel vrage ranunkel kalla hellebard cirkel tejne grandezza nippe hæld strikke militær tretten vildt rakker bisam lude viol ose aspidistra fersken stritte absolut kollekt lak kravle snitte smide gønge vinde tremme klerk tviste tit mangle sejre strube monolit stakater glane"
 const words = wordsString.split(' ')
-const  textP = document.querySelector('p#text')
+const textP = document.querySelector('p#text')
 
 //word state
-let wordsIndex = 0
-let currentWordsIndex = 1
-const currentLetters = []
-
-reloadText()
+let wordsIndex
+let currentWordsIndex
 
 //setup text input
 const input = document.querySelector('input#input')
-input.addEventListener('keyup', textInput)
 
 //setup countdown
-let countdown = 60
 const countdownP = document.querySelector('p#countdown')
+let countdown
+let countdownInterval
 
-const countdownInterval = setInterval(reloadCountdown, 1000)
+//highscore state
+let wordsTyped
+let correctWordsTyped
+
+function defaultValues() {
+  countdown = 60
+
+  wordsIndex = 0
+  currentWordsIndex = 1
+
+  wordsTyped = 0
+  correctWordsTyped = 0
+
+  countdownP.textContent = countdown
+}
+
+function idleState() {
+  defaultValues()
+  reloadText()
+  selectNextWord()
+
+  input.setAttribute('placeholder', 'Type to begin.')
+  input.addEventListener('keyup', gameState)
+}
+
+function gameState() {
+  const infoH3 = document.querySelector('h3#info')
+  infoH3.textContent = "Game On!"
+
+  input.removeAttribute('placeholder')
+  input.removeEventListener('keyup', gameState)
+
+  //calling reloadCountdown once before set interval to avoid delay
+  reloadCountdown()
+  countdownInterval = setInterval(reloadCountdown, 1000)
+
+
+  input.addEventListener('keyup', textInput)
+}
 
 function reloadText() {
   const currentWords = words.slice(wordsIndex, wordsIndex + 5)
-  const nodes = textP.childNodes
-  nodes.forEach(node => node.remove())
 
   currentWords.forEach(word => {
     const span = document.createElement('span')
@@ -35,6 +75,7 @@ function reloadText() {
   currentWordsIndex = 1
 }
 
+//inserts the next text to be typed
 function reloadCountdown() {
   countdownP.textContent = countdown
 
@@ -42,27 +83,66 @@ function reloadCountdown() {
 
   if (countdown < 0) {
     clearInterval(countdownInterval)
+    timesUp()
   }
+}
+
+function timesUp() {
+  //remove focus and event listener for text input
+  input.blur()
+  input.removeEventListener('keyup', textInput)
+
+  const infoh3 = document.querySelector('h3#info')
+  infoh3.textContent = 'Times up!'
+
+
+  //update highscore table
+  const newScoreRow = document.createElement('tr')
+
+  const newScoreSubmitted = document.createElement('td')
+  const date = new Date()
+  const day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate()
+  const month = date.getMonth() < 10 ? '0' + date.getMonth() : date.getMonth()
+  const year = date.getFullYear()
+  newScoreSubmitted.textContent = `${day}/${month}/${year}`
+
+  const newScoreWordsTyped = document.createElement('td')
+  newScoreWordsTyped.textContent = wordsTyped
+
+  const newScoreCorrectWordsTyped = document.createElement('td')
+  newScoreCorrectWordsTyped.textContent = correctWordsTyped
+
+  newScoreRow.appendChild(newScoreSubmitted)
+  newScoreRow.appendChild(newScoreWordsTyped)
+  newScoreRow.appendChild(newScoreCorrectWordsTyped)
+
+  const highscoreTableBody = document.querySelector('table#highscore tbody')
+  highscoreTableBody.insertBefore(newScoreRow, highscoreTableBody.firstChild)
+
+  removeTextChildren()
+  idleState()
 }
 
 function textInput(event) {
   const key = event.key
 
-  if (key != ' ') {
-    currentLetters.push(key)    
-  } else {
+   if (key === ' '){
+    wordsTyped++
+
     //collects the typed word into a string
-    const typedWord = currentLetters.join('')
+    const typedWord = input.value.trim()
     const isCorrect = typedWord === words[wordsIndex]
+    if (isCorrect) {
+      correctWordsTyped++
+    }
 
     //attaches the correct class to a word span
-    const wordClass = isCorrect ? 'correct' : 'wrong'
+    const wordClass = isCorrect ? 'correct' : 'incorrect'
     const currentWordSpan = document.querySelector('p#text span:nth-child(' + currentWordsIndex + ')')
     currentWordSpan.classList.add(wordClass)
 
     //resets the state
     input.value = ''
-    currentLetters.splice(0, currentLetters.length)
 
     //advances the current words
     advanceWords()
@@ -72,9 +152,26 @@ function textInput(event) {
 function advanceWords() {
   currentWordsIndex++
   wordsIndex++
+
   if (currentWordsIndex > 5) {
     currentWordsIndex = 1
+    removeTextChildren()
     reloadText()
   }
 
+  selectNextWord()
+}
+
+//will remove the old selection and add the new one
+function selectNextWord() {
+  const previousSelection = document.querySelector('span.selected')
+  if (previousSelection) {
+    previousSelection.classList.remove('selected')
+  }
+  document.querySelector('p#text span:nth-child(' + currentWordsIndex + ')').classList.add('selected')
+}
+
+//removes the old text
+function removeTextChildren() {
+  textP.querySelectorAll('*').forEach(n => n.remove())
 }
